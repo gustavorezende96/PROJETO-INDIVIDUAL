1ac8cc5bf08c921d66d7205c56c3a1bbfaac1b86
Gustavo Rezende <gustavo.silva@sptech.school>
2022-05-12 14:31:34 -0300

    Alerts das validações arru

--- Project/js/cadastro.js
+++ Project/js/cadastro.js
@@ -4,35 +4,73 @@ function entrar(){
     var eemail = email.value;
     var password = senha.value;
 
-    if(nomee.length == 0){    
+    //VALIDAÇÕES DO CADASTRO
+
+        //CAMPOS NÃO PREENCHIDOS
+    if(nomee.length == 0 && eemail.length == 0 && password.length == 0){
+        Swal.fire({
+            icon: 'info',
+            title: 'Insira as informações',
+            text: 'Por favor, preencha os campos!',
+        })
+    }
+
+        //NOME NÃO PREENCHIDO
+    else if(nomee.length == 0){    
         Swal.fire({
         icon: 'info',
         title: 'Oops...',
-        text: 'Você se esqueceu de informar seu nome!',
+        text: 'Você se esqueceu de informar seu nome!'
     })
     }
+
+        //EMAIL NÃO PREENCHIDO
+    else if(eemail.length == 0){
+        Swal.fire({
+            icon: 'info',
+            title: 'E-mail não informado!',
+            text: 'Por favor, insira um e-mail!'
+        })
+    }
+
+        //EMAIL INVÁLIDO "@"
     else if(eemail.indexOf("@") == -1){
         Swal.fire({
             icon: 'info',
             title: 'E-mail inválido!',
-            text: 'Por favor, verifique se o e-mail inserido é um email válido!',
+            text: 'Por favor, verifique se o e-mail inserido é um email válido!'
         })
     }
+
+        //EMAIL INVÁLIDO "."
+    else if(eemail.indexOf(".") == -1){
+        Swal.fire({
+            icon: 'info',
+            title: 'E-mail inválido!',
+            text: 'Por favor, verifique se o e-mail inserido é um email válido!'
+        })
+    }
+
+        //SENHA NÃO PREENCHIDA
     else if(password.length == 0){
         Swal.fire({
             icon: 'error',
             title: 'Senha obrigatória!',
-            text: 'Por favor, insira uma senha!',
+            text: 'Por favor, insira uma senha!'
         })
     }
+
+        //SENHA COM MENOS DE 5 CARACTÉRES
     else if(password.length < 5){
         Swal.fire({
             icon: 'info',
             title: 'Senha fraca!',
-            text: 'Uma senha forte deve conter no minímo 5 caractéres.',
+            text: 'Uma senha deve conter no minímo 5 caractéres.'
         })
         }
-        else{
+        
+        //SUCESSO
+    else{
             Swal.fire({
                 position: 'top-end',
                 icon: 'success',
